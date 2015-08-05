64fcb669ee006c798ce2b4464dd19ce7c300a91c
Michael Osborn <joebob60@hotmail.com>
2015-08-04 17:46:14 -0700

    simple_cms project

---
+++ Sites/simple_cms/app/controllers/sections_controller.rb
@@ -0,0 +1,46 @@
+class SectionsController < ApplicationController
+
+  layout false
+  
+  def index
+    @sections = Section.sorted
+  end
+
+  def show
+    @section = Section.find(params[:id])
+  end
+
+  def new
+    @section = Section.new({:name => "Default"})
+  end
+
+  def create
+    @section = Section.new(section_params)
+    if @section.save
+      flash[:notice] = "Section created successfully."
+      redirect_to(:action => "index")
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @section = Section.find(params[:id])
+  end
+
+  def delete
+    @section = Section.find(params[:id])
+  end
+
+  def destroy
+    section = Section.find(params[:id]).destroy
+    flash[:notice] = "Section destroyed successfully."
+    redirect_to(:action => 'index')
+  end
+
+private
+  def page_params
+    params.require(:section).permit(:page_id, :name, :position, :visible, :content_type, :content)
+  end
+
+end
